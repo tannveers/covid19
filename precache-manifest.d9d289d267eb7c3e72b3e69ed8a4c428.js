8d855043385bbffb89407fc4efd57d790639d611
tannveers <tanveer9812@gmail.com>
2020-07-01 16:33:46 +0530

    Updates

---
+++ precache-manifest.d9d289d267eb7c3e72b3e69ed8a4c428.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a85103aa228ecae1b0ed1f92a4b8e3d",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "b2567a14ef4d255141f5",
+    "url": "/covid19/static/css/main.d0dd3d81.chunk.css"
+  },
+  {
+    "revision": "70c2959d37605e4cb004",
+    "url": "/covid19/static/js/2.d1a60a6d.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.d1a60a6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2567a14ef4d255141f5",
+    "url": "/covid19/static/js/main.5e9df958.chunk.js"
+  },
+  {
+    "revision": "6aa52515bbe8ece9a2e7",
+    "url": "/covid19/static/js/runtime-main.50d6825a.js"
+  }
+]);
